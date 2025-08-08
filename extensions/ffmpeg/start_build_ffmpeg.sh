aff84098dfcc8a32da37524b6737a2f93a491e55
SmileChaoLee <leechao1959@gmail.com>
2025-08-08 12:44:20 -0400

    Commit files that were not done before

--- extensions/ffmpeg/start_build_ffmpeg.sh
+++ extensions/ffmpeg/start_build_ffmpeg.sh
@@ -4,15 +4,19 @@
 
 FFMPEG_EXT_PATH="$(pwd)/src/main"
 echo ${FFMPEG_EXT_PATH}
-NDK_PATH="/home/chaolee/Android/Sdk/ndk/22.1.7171670"
+# NDK_PATH="/home/chaolee/Android/Sdk/ndk/22.1.7171670"
+NDK_PATH="/home/chaolee/Android/Sdk/ndk/26.3.11579264"
 echo ${NDK_PATH}
 HOST_PLATFORM="linux-x86_64"
 echo ${HOST_PLATFORM}
-ENABLED_DECODERS=(vorbis opus flac alac pcm_mulaw pcm_alaw aac mp3 amrnb amrwb aac ac3 eac3 dca mlp truehd)
+ANDROID_ABI="23"
+echo ${ANDROID_ABI}
+ENABLED_DECODERS=(vorbis opus flac alac pcm_mulaw pcm_alaw aac mp3 amrnb amrwb ac3 eac3 dca mlp truehd)
+# ENABLED_DECODERS=(vorbis opus flac mp3)
 
 cd "${FFMPEG_EXT_PATH}/jni" && \
 ./build_ffmpeg.sh \
-  "${FFMPEG_EXT_PATH}" "${NDK_PATH}" "${HOST_PLATFORM}" "${ENABLED_DECODERS[@]}"
+  "${FFMPEG_EXT_PATH}" "${NDK_PATH}" "${HOST_PLATFORM}" "${ANDROID_ABI}" "${ENABLED_DECODERS[@]}"
 
 echo "make clean --> finished"
 echo " "
