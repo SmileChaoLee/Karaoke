0f146f8e3049be5b4b53565c1c230fdc877f2ca8
SmileChaoLee <leechao1959@gmail.com>
2026-07-07 12:42:51 -0400

    Changed the search term

--- app/src/u2bkaraoke/java/com/smile/u2bkaraoke/fragments/SongListFragment.kt
+++ app/src/u2bkaraoke/java/com/smile/u2bkaraoke/fragments/SongListFragment.kt
@@ -698,7 +698,7 @@ class SongListFragment : U2bKKBaseFragment(), RecyclerItemListener {
         if (song.singer2Na.isNotEmpty() && song.singer2Na.uppercase() != "UNKNOWN") {
             searchTerm = searchTerm + " " + song.singer2Na.trim()
         }
-        searchTerm = " $searchTerm, 有人聲"
+        // searchTerm = " $searchTerm KTV"
 
         LogUtil.d(TAG, "songSearchTerm.searchTerm = $searchTerm")
         return searchTerm
